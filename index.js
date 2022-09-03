fc6737371f1e275bde89d1a2917691e405067408
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2022-09-03 20:33:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const math = require('math')
+
+// points should ne in format, f.ex: [[1,2],[3,4],[5,6],..]
+function outliers2D (points, _sigma) {
+  const sigma = _sigma || 3
+  const medianPoint = [math.median(points.map(p => p[0])), math.median(points.map(p => p[1]))]
+
+  // remove outliers with Median Absolute Deviation (MAD)
+  const madValue = [math.mad(points.map(p => p[0])), math.mad(points.map(p => p[1]))]
+  const mad2d = math.sqrt(math.square(madValue[0]) + math.square(madValue[1]))
+
+  const strippedPoints = []
+  const fliteredPoints = points.filter(pt => {
+    if (
+      (pt[0] - medianPoint[0] < sigma * madValue[0]) &&
+      (pt[1] - medianPoint[1] < sigma * madValue[1]) &&
+      (math.sqrt(math.square(pt[0] - medianPoint[0]) + math.square(pt[1] - medianPoint[1])) < sigma * mad2d)
+    ) {
+      return true
+    } else {
+      strippedPoints.push(pt)
+      return false
+    }
+  })
+
+  return { fliteredPoints, strippedPoints }
+}
+
+module.export = outliers2D
